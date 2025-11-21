38794be3e57a26cbe29e4edba4d46efcf62d529b
Your Name <alisapodgaynaya023@gmail.com>
2025-11-21 13:31:48 -0700

    practice TS tasks 1-9

---
+++ src/task-2.ts
@@ -0,0 +1,11 @@
+type Currency = "USD" | "UAH" | "EUR";
+interface ConvertCurrencyParams {
+    amount: number;
+    currency: Currency;
+}
+
+function convertCurrency({ amount,   currency  }:ConvertCurrencyParams): void {
+  console.log(`Converting ${amount} to ${currency}`);
+}
+
+convertCurrency({ amount: 100, currency: "UAH" });
---
+++ src/task-8.ts
@@ -0,0 +1,29 @@
+
+
+enum Role {
+  Admin,
+  User,
+  Guest
+}
+
+function getPermissions(role: Role): string[] {
+    switch (role) {
+        case Role.Admin:
+            return ['create', 'read', 'update', 'delete'];
+    
+        case Role.User:
+            return ['create', 'read'];
+    
+        case Role.Guest:
+            return ['read'];
+        default:
+            return [];   
+    }
+     
+}
+
+console.log(getPermissions(Role.Admin));
+
+console.log(getPermissions(Role.User));
+
+console.log(getPermissions(Role.Guest));
